dde26929b95e3aa2ffd2c6e755a72a368260971e
d-ote <dooterior@gmail.com>
2021-07-29 11:29:48 -0700

    loginPage setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
+import Dashboard from './components/dashboard';
+import LoginPage from './components/loginPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Route path="/" exact component={ LoginPage } />  
+      <Route path="/dashboard" exact component ={ Dashboard } />  
+    </Router>
   );
 }
 
